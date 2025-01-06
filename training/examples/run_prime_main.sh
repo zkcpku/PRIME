b0eb00bcd6353f051e5aad689d04762475385314
Lifan Yuan <68536405+lifan-yuan@users.noreply.github.com>
2025-01-05 23:27:10 -0800

    fix bash file
    add ray

--- training/examples/run_prime_main.sh
+++ training/examples/run_prime_main.sh
@@ -12,6 +12,13 @@ DATA_PATH=path/to/data
 SFT_MODEL_PATH=PRIME-RL/Eurus-2-7B-SFT
 CKPT_PATH=path/to/save/dir
 
+port=6379
+ray start --head \
+    --port=$port \
+    --num-gpus=8 \
+    --include-dashboard=false \
+    --block &
+
 python3 -m verl.trainer.main_ppo \
     data.train_files=["$DATA_PATH/train.parquet"] \
     data.val_files=["$DATA_PATH/validation.parquet"] \
@@ -47,20 +54,19 @@ python3 -m verl.trainer.main_ppo \
     data.filter_accuracy=True \
     data.accuracy_lower_bound=0.2 \
     data.accuracy_upper_bound=0.8 \
-    algorithm.adv_estimator=rloo \
-    algorithm.adv_params.verifier_gamma=1.0 \
-    algorithm.adv_params.reward_model_gamma=1.0 \
-    reward_model.rm_type=prime \
-    reward_model.rm_coef=5 \
-    reward_model.prime_model.path=$SFT_MODEL_PATH  \
-    reward_model.prime_model.ref_path=$SFT_MODEL_PATH  \
+    +algorithm.adv_params.verifier_gamma=1.0 \
+    +algorithm.adv_params.reward_model_gamma=1.0 \
+    +reward_model.rm_type=prime \
+    +reward_model.rm_coef=5 \
+    +reward_model.prime_model.path=$SFT_MODEL_PATH  \
+    +reward_model.prime_model.ref_path=$SFT_MODEL_PATH  \
     reward_model.model.input_tokenizer=null \
-    reward_model.prime_granularity=token \
+    +reward_model.prime_granularity=token \
     reward_model.micro_batch_size=8 \
-    reward_model.prime_model.update=after \
-    reward_model.prime_model.beta_train=0.05 \
-    reward_model.prime_model.optim.lr=1e-6 \
-    reward_model.prime_model.optim.grad_clip=10.0 \
-    reward_model.prime_model.input_tokenizer=null \
+    +reward_model.prime_model.update=after \
+    +reward_model.prime_model.beta_train=0.05 \
+    +reward_model.prime_model.optim.lr=1e-6 \
+    +reward_model.prime_model.optim.grad_clip=10.0 \
+    +reward_model.prime_model.input_tokenizer=null \
     trainer.default_local_dir=$CKPT_PATH/$PROJECT_NAME/$EXPERIMENT_NAME \
 
